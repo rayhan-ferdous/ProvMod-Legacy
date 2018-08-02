4c95d2143c8a0d616abb6a1f2cc745fcd5add56e
Rayhan Ferdous <aaponcseku@gmail.com>
2018-08-02 16:38:27 -0600

    architecture extraction ready

---
+++ archi parsing/graphgen_dup.py
@@ -0,0 +1,49 @@
+#call graph with duplicate
+import py2neo as pn
+graph = pn.Graph(password = 'password')
+graph.delete_all()
+
+
+xml = open('injection.xml')
+stack = []
+nodes = set()
+
+for line in xml:
+    if '<crhm>' in line:
+        stack.append('crhm')
+        nodes.add('crhm')
+        q = 'create(n:System{name: "crhm"})'
+        graph.run(q)
+        #print stack
+
+    elif '<\crhm>' in line:
+        stack.pop()
+        #print stack
+
+    elif '</' not in line:
+        stack.append(line.strip()[1 : len(line)-3])
+
+        parent = stack[len(stack)-2]
+        child = stack[len(stack)-1]
+
+
+        #print stack
+
+        #print child
+
+        if child not in nodes:
+            nodes.add(child)
+            q1 = 'create(n:Fn{name:"' + child + '"})'
+            graph.run(q1)
+
+        # to remove multiple calls indent this scope inside if
+        q2 = 'match (p), (c) where p.name ="' + parent + '" and c.name ="' + child + '" create (p)-[r:calls]->(c)'
+        graph.run(q2)
+        # to remove multiple calls indent this scope inside if
+    else:
+        stack.pop()
+        #print stack
+
+
+
+print stack
